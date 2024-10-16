2f346f5365f8f5eec6303373eee95da5856d3d84
Vardkor <theoarnaud55@gmail.com>
2024-10-16 20:52:51 +0200

    [GPP]_TH
    16/10/2024 : interaction fonctionnel, mais sans le rigidbody encore

--- Assets/Make A Mess/Main/Interaction/Interaction.cs
+++ Assets/Make A Mess/Main/Interaction/Interaction.cs
@@ -5,29 +5,42 @@ using UnityEngine;
 public class Interaction : MonoBehaviour
 {
 
-    public Transform GrabMask;
-
-    public bool Grabed = false;
-
+    public Transform GrabMask;  
+    public bool Grabed = false; 
+    private Transform grabbedObject;
     private void Update()
     {
         RaycastHit hit;
 
         Debug.DrawRay(transform.position, transform.forward * 10, Color.red);
 
-        if (Physics.Raycast(transform.position, transform.forward, out hit, 10))
+        if (!Grabed) 
         {
-            if (hit.collider.CompareTag("Grab"))
+            if (Physics.Raycast(transform.position, transform.forward, out hit, 10))
             {
-                if (Input.GetKeyDown(KeyCode.E))
+                if (hit.collider.CompareTag("Grab"))
                 {
-                    hit.transform.position = (GrabMask.position);
-                    Grabed = true;
-                    //Debug.Log("Tu as grab l'objet");
+                    if (Input.GetKeyDown(KeyCode.E))
+                    {
+                        grabbedObject = hit.transform;  
+                        grabbedObject.position = GrabMask.position; 
+                        grabbedObject.SetParent(GrabMask); 
+                        Grabed = true; 
+                        //rb.isKinematic = true;
+                    }
                 }
+            }
+        }
+        else 
+        {
+            if (Input.GetKeyDown(KeyCode.E)) 
+            {
+                grabbedObject.SetParent(null); 
+                grabbedObject = null; 
+                Grabed = false; 
+
 
             }
-           
         }
     }
 }
