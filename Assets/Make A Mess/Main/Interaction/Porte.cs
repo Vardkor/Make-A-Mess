25318f779d688c7eaa812c18a98824f2da586b35
Vardkor <theoarnaud55@gmail.com>
2024-10-22 12:38:55 +0200

    [GPP]_TH
    22/10/2024 : Ajout du script porte

--- Assets/Make A Mess/Main/Interaction/Porte.cs
+++ Assets/Make A Mess/Main/Interaction/Porte.cs
@@ -4,25 +4,69 @@ using UnityEngine;
 
 public class Porte : MonoBehaviour
 {
-    private bool OpenPorte;
-    void OnTriggerStay(Collider other)
-    { 
-        OpenPorte = true; 
-    }
-    void OnTriggerExit(Collider other)
-    {        
-        OpenPorte = false;
-    }
+
+    [SerializeField] GameObject key1;
+    [SerializeField] GameObject key2;
+    [SerializeField] GameObject key3;
+    [SerializeField] GameObject key4;
+    [SerializeField] GameObject key5;
+    [SerializeField] GameObject Door;
+
+    public bool key1T = false;
+    public bool key2T = false;
+    public bool key3T = false;
+    public bool key4T = false;
+    public bool key5T = false;
+
+    public bool porteOuverte = false;
+    
     void Update()
     {
-        if (OpenPorte == true)
+        RaycastHit hit;
+
+        //Debug.DrawRay(transform.position, transform.forward * 10, Color.red);
+
+        if (Physics.Raycast(transform.position, transform.forward, out hit, 10))
         {
-            transform.Rotate(0, 90f, 0);
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                if (hit.collider.CompareTag("Key"))
+                {
+                    key1T = true;
+                    Destroy(hit.collider.gameObject);
+                }
+                else if (hit.collider.CompareTag("Key2"))
+                {
+                    key2T = true; 
+                    Destroy(hit.collider.gameObject);
+                }
+                else if (hit.collider.CompareTag("Key3"))
+                {
+                    key3T = true;
+                    Destroy(hit.collider.gameObject);
+                }
+                else if (hit.collider.CompareTag("Key4"))
+                {
+                    key4T = true;
+                    Destroy(hit.collider.gameObject);
+                }
+                else if (hit.collider.CompareTag("Key5"))
+                {
+                    key5T = true;
+                    Destroy(hit.collider.gameObject);
+                }
+            }
         }
-        else if(OpenPorte == false)
+        if (key1T && key2T && key3T && key4T && key5T && !porteOuverte)
         {
-           transform.Rotate(0, 0, 0); 
+
+            OpenDoor();
         }
     }
 
+    void OpenDoor()
+    {
+        porteOuverte = true;
+        Door.transform.Rotate(0, 90f, 0);
+    }
 }
