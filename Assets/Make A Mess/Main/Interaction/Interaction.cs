dc1eacce3ab2ba0b5264da8a8ee3a76b85d6c4dd
Vardkor <theoarnaud55@gmail.com>
2024-10-22 23:12:06 +0200

    [GPP]_TH
    22/10/2024 : ajout du système de particule, test encore a faire ( pas fini )

--- Assets/Make A Mess/Main/Interaction/Interaction.cs
+++ Assets/Make A Mess/Main/Interaction/Interaction.cs
@@ -4,67 +4,142 @@ using UnityEngine;
 
 public class Interaction : MonoBehaviour
 {
-
+    [SerializeField] private GameObject extincteurA; 
+    [SerializeField] private ParticleSystem extincteurParticles;
     public Transform GrabMask;  
     public bool Grabed = false; 
-    private Transform grabbedObject;
-    public float forcelancer = 50f;
+    public bool HasExtincteur = false;
+    private Transform grabbedObject; 
+    public float forcelancer = 50f; 
+    
+
+    void Start()
+    {
+        extincteurA.SetActive(false); 
+    }
+
     private void Update()
     {
-        RaycastHit hit;
-
-        //Debug.DrawRay(transform.position, transform.forward * 10, Color.red);
-
-        if (!Grabed) 
+        
+        if (!Grabed)
         {
+            RaycastHit hit;
             if (Physics.Raycast(transform.position, transform.forward, out hit, 10))
             {
+                
                 if (hit.collider.CompareTag("Grab"))
+                {
+                    if (Input.GetKeyDown(KeyCode.E)) 
+                    {
+                        GrabObject(hit.transform);
+                    }
+                }
+                
+                else if (hit.collider.CompareTag("Extincteur"))
                 {
                     if (Input.GetKeyDown(KeyCode.E))
                     {
-                        grabbedObject = hit.transform;  
-                        grabbedObject.position = GrabMask.position; 
-                        grabbedObject.SetParent(GrabMask); 
-                        Grabed = true; 
-                        Rigidbody rb = grabbedObject.GetComponent<Rigidbody>();
-                        if (rb != null)
-                        {
-                            rb.isKinematic = true;
-                        }
+                        GrabExtincteur(hit.collider.gameObject);
                     }
                 }
             }
         }
-        else 
-        {
-            if (Input.GetKeyDown(KeyCode.E)) 
+        else
+        { 
+            if (Input.GetKeyDown(KeyCode.E))
             {
-                Rigidbody rb = grabbedObject.GetComponent<Rigidbody>();
-                if (rb != null)
-                {
-                    rb.isKinematic = false;
-                }
-                
-                grabbedObject.SetParent(null); 
-                grabbedObject = null; 
-                Grabed = false; 
-                
-
+                ReleaseObject();
             }
+            
             else if (Input.GetKeyDown(KeyCode.F))
             {
-                Rigidbody rb = grabbedObject.GetComponent<Rigidbody>();
-                if (rb != null)
+                LaunchObject();
+            }
+
+            if(Grabed == true && HasExtincteur == true)
+            {
+                if(Input.GetKey(KeyCode.Mouse0))
                 {
-                    rb.isKinematic = false;
-                    rb.AddForce(transform.forward * forcelancer, ForceMode.Impulse);
+                    if (!extincteurParticles.isPlaying)
+                    {
+                        extincteurParticles.Play();
+                        Grabed = true;
+                        HasExtincteur = true;
+                    }
+                    else if (extincteurParticles.isPlaying)
+                    {
+                        extincteurParticles.Stop();
+                    }
                 }
-                
-                grabbedObject.SetParent(null);
-                grabbedObject = null; 
-                Grabed = false;
-                
+            }
+        }
+    }
+
+    
+    private void GrabObject(Transform objectToGrab)
+    {
+        grabbedObject = objectToGrab;
+        grabbedObject.position = GrabMask.position; 
+        grabbedObject.SetParent(GrabMask); 
+        Grabed = true; 
+
+        Rigidbody rb = grabbedObject.GetComponent<Rigidbody>();
+        if (rb != null)
+        {
+            rb.isKinematic = true; 
+        }
+    }
+
+    
+    private void GrabExtincteur(GameObject extincteur)
+    {
+        Destroy(extincteur); 
+        extincteurA.SetActive(true); 
+        Grabed = true; 
+        HasExtincteur = true;
+    }
+
+        private void ReleaseObject()
+    {
+        if (grabbedObject != null)
+        {
+            Rigidbody rb = grabbedObject.GetComponent<Rigidbody>();
+            if (rb != null)
+            {
+                rb.isKinematic = false; 
+            }
+
+            grabbedObject.SetParent(null); 
+            grabbedObject = null; 
+            Grabed = false; 
+        }
+    }
+
+    
+    private void LaunchObject()
+    {
+        if (grabbedObject != null)
+        {
+            Rigidbody rb = grabbedObject.GetComponent<Rigidbody>();
+            if (rb != null)
+            {
+                rb.isKinematic = false; 
+                rb.AddForce(transform.forward * forcelancer, ForceMode.Impulse); 
+            }
+
+            grabbedObject.SetParent(null); 
+            grabbedObject = null; 
+            Grabed = false; 
+        }
+    }
+
+    void ShootExtincteur()
+    {
+        if(Grabed == true && HasExtincteur == true)
+        {
+            if(Input.GetKeyDown(KeyCode.E))
+            {
+                Debug.Log("Objet Attraper en main");
             }
         }
     }
