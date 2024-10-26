812736e02d2447fa3043608fb08d188cff20149e
Vardkor <theoarnaud55@gmail.com>
2024-10-26 17:11:51 +0200

    [GPP]_TH
    26/10/2024 : ajout d'un script pour le scoring mais pas fini

---
+++ Assets/Make A Mess/Main/System/Scorring.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scorring : MonoBehaviour
+{
+
+    public int Score = 0;
+    public static ScoreManager instance;
+
+    void Update()
+    {
+        Debug.Log("Le score est de : " + Score);
+    }
+}
