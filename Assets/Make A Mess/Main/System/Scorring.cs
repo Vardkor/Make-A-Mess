9a98caf32bed2c06fd85abcdf7ceb1e1ae5ce4a7
Vardkor <theoarnaud55@gmail.com>
2024-11-18 17:00:09 +0100

    [GPP]_TH
    18/11/2024 : test pour le score

--- Assets/Make A Mess/Main/System/Scorring.cs
+++ Assets/Make A Mess/Main/System/Scorring.cs
@@ -6,15 +6,28 @@ public class Scorring : MonoBehaviour
 {
 
     public int Score = 0;
+    public GameObject[] objets = new GameObject[10];
+    public string Cible;
 
     void Update()
     {
-        //if()
-        //{
-            //if(gameObject.layer)
-            //{
-            //    Debug.Log("Le score est de : " + Score);
-            //}
-        //}
+        RaycastHit hit;
+
+        if(Physics.Raycast(transform.position, transform.forward, out hit, 10))
+        {
+            if(hit.collider.CompareTag("Scrore100"))
+            {
+                foreach (GameObject obj in objets) 
+                {
+                    if(obj.name == Cible)
+                    {
+                        Debug.Log("Ajout de 100 au score");
+                        Score =+ 100;
+                        Debug.Log("" + Score);
+                        break;
+                    }
+                }
+            }
+        }
     }
 }
