3d843226279874a3d24648cb7fc757008b717343
Vardkor <theoarnaud55@gmail.com>
2025-01-30 18:10:54 +0100

    [GPP]_TH
    30/01/2025 : qq test sur le score

--- Assets/Make A Mess/Main/Script/System/ScorringManager.cs
+++ Assets/Make A Mess/Main/Script/System/ScorringManager.cs
@@ -18,39 +18,23 @@ public class ScorringManager : MonoBehaviour
     
     //Multilier Timer\\
 
-    private float multiplierTimer = 0f;
+    private float multiplierTimer = 5f;
     private float multiplierDuration = 5f;
-    private int multiplierScore = 0;
 
     void Start()
     {
         UpdateText();
     }
 
-    void Update()
-    {
-        if(multiplier > 1f)
-        {
-            multiplierTimer -= Time.deltaTime;
-            if(multiplierTimer <= 0f)
-            {
-                multiplier = multiplierBase;
-                Debug.Log("Le multipliers" + multiplier);
-            }
-        }
-    }
-
-
     public void AddScore(int scoreToAdd)
     {
-        scoreToAdd *= multiplier = multiplierScore;
         UpdateScore(scoreToAdd);
-        //ActiveMultipliers();
+        ActiveMultipliers();
     }
 
     private void UpdateScore(int scoreToAdd)
     {
-        CurrentScore += multiplierScore;
+        CurrentScore += scoreToAdd;
         UpdateText();
         SFXCling.pitch = Random.Range(0.9f,1.1f); 
         SFXCling.Play();
@@ -62,4 +46,9 @@ public class ScorringManager : MonoBehaviour
             scoreText.text = CurrentScore + "$";
         }
     }
+
+    public void ActiveMultipliers()
+    {
+        
+    }  
 }
