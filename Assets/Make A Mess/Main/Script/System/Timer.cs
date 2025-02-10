2a4fbf5adeb96764e1ef7a9f1e19723b3582693a
HiItsFeu <l.gurget17@gmail.com>
2025-02-10 21:18:23 +0100

    [GPP][UI]_LG
    10/02/25 : ajout barre police + affichages des messages en bas à gauche

--- Assets/Make A Mess/Main/Script/System/Timer.cs
+++ Assets/Make A Mess/Main/Script/System/Timer.cs
@@ -2,12 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class Timer : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI timer;
-    [SerializeField] float remainingTime = 300;
     [SerializeField] public AudioSource Notif;
     [SerializeField] public AudioSource MainMusic;
     [SerializeField] Objet scriptobjet;
@@ -15,11 +15,19 @@ public class Timer : MonoBehaviour
     [SerializeField] private GameObject GameOverScreen;
     [SerializeField] private GameObject GameWinScreen;
 
+    private float maxTime = 600f;
+    public float remainingTime;
+
+    [SerializeField] Slider slidersCops;
+
     public bool StartTimer;
     public bool HasPlay;
     public bool PlayerExitMusee = false;
     private bool gameHasEnded = false;
 
+    private float duration = 6f;
+
+    [Header("Message On PC")]
     public GameObject Message1;
     public GameObject Message2;
     public GameObject Message3;
@@ -27,10 +35,24 @@ public class Timer : MonoBehaviour
     public GameObject Message5;
     public GameObject Message6;
     public GameObject Message7;
+
+    [Header("In Game Message")]
+    public GameObject IGMessage1;
+    public GameObject IGMessage2;
+    public GameObject IGMessage3;
+    public GameObject IGMessage4;
+    public GameObject IGMessage5;
+    public GameObject IGMessage6;
+    public GameObject IGMessage7;
+
+    private GameObject currentMessage;
+    private GameObject currentIGMessage;
+
     public GameObject NotifUIPC;
     
     public void Start()
     {
+        maxTime = remainingTime;
         StartTimer = false;
         gameHasEnded = false;
     }
@@ -40,51 +62,59 @@ public class Timer : MonoBehaviour
         if(StartTimer)
         {
             remainingTime -= Time.deltaTime;
+            UpdateSlider();
+
             int minutes = Mathf.FloorToInt(remainingTime / 60);
             int seconds = Mathf.FloorToInt(remainingTime % 60);
             int centiseconds = Mathf.FloorToInt((remainingTime - Mathf.Floor(remainingTime)) * 100);
-            timer.text = string.Format("{0:00}:{1:00}:{0:00}", minutes, seconds, centiseconds);
-
+            timer.text = string.Format("{0:00}:{1:00}:{2:00}", minutes, seconds, centiseconds);
             
-            if(minutes == 4 && seconds == 30)
+            if(minutes == 9 && seconds == 50)
             {
-                Message1.SetActive(true);
+                ActiveUI(Message1);
+                ActiveInGameUI(IGMessage1);
                 NotifUIPC.SetActive(true);
                 Notif.Play();
             }
             if(minutes == 4 && seconds == 0)
             {
-                Message2.SetActive(true);
+                ActiveUI(Message2);
+                ActiveInGameUI(IGMessage2);
                 NotifUIPC.SetActive(true);
                 Notif.Play();
             }
             if(minutes == 3 && seconds == 30)
             {
-                Message3.SetActive(true);
+                ActiveUI(Message3);
+                ActiveInGameUI(IGMessage3);
                 NotifUIPC.SetActive(true);
                 Notif.Play();
             }
             if(minutes == 3 && seconds == 0)
             {
-                Message4.SetActive(true);
+                ActiveUI(Message4);
+                ActiveInGameUI(IGMessage4);
                 NotifUIPC.SetActive(true);
                 Notif.Play();
             }
             if(minutes == 1 && seconds == 30)
             {
-                Message5.SetActive(true);
+                ActiveUI(Message5);
+                ActiveInGameUI(IGMessage5);
                 NotifUIPC.SetActive(true);
                 Notif.Play();
             }
             if(minutes == 1 && seconds == 0)
             {
-                Message6.SetActive(true);
+                ActiveUI(Message6);
+                ActiveInGameUI(IGMessage6);
                 NotifUIPC.SetActive(true);
                 Notif.Play();
             }
             if(minutes == 0 && seconds == 45)
             {
-                Message7.SetActive(true);
+                ActiveUI(Message7);
+                ActiveInGameUI(IGMessage7);
                 NotifUIPC.SetActive(true);
                 Notif.Play();
             }
@@ -127,6 +157,59 @@ public class Timer : MonoBehaviour
             StartTimer = true;
             HasPlay = true;
             StartingTimer(); 
+        }
+    }
+
+    void UpdateSlider()
+    {
+        if (slidersCops != null)
+        {
+            slidersCops.value = remainingTime;
+        }
+    }
+
+    void ActiveUI(GameObject ui)
+    {
+        if (currentMessage != null && currentMessage != ui)
+        {
+            currentMessage.SetActive(false);
+        }
+
+        if (ui != null)
+        {
+            ui.SetActive(true);
+            currentMessage = ui;
+        }
+    }
+
+    void ActiveInGameUI(GameObject IGui)
+    {
+        if (currentIGMessage != null && currentIGMessage != IGui)
+        {
+            currentIGMessage.SetActive(false);
+        }
+
+        if (IGui != null)
+        {
+            IGui.SetActive(true);
+            CancelInvoke(nameof(DesactivateCurrentUI));
+            Invoke(nameof(DesactivateCurrentUI), duration);
+            currentIGMessage = IGui;
+        }
+    }
+
+    void DesactivateCurrentUI()
+    {
+        if (currentMessage != null)
+        {
+            currentMessage.SetActive(false);
+            currentMessage = null;
+        }
+
+        if(currentIGMessage != null)
+        {
+            currentIGMessage.SetActive(false);
+            currentIGMessage = null;
         }
     }
 }
